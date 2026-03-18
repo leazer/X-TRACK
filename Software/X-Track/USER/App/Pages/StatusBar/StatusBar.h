c6789397a112aed016c80142d52ff1007e1fe05f
zealerlu <zealerlu@foxmail.com>
2026-03-19 02:01:25 +0800

    Revert "refactor: move StatusBar ui struct into StatusBar class"
    This reverts commit 293d2ceef646f790358ed89ac4187064cce56d35.

--- Software/X-Track/USER/App/Pages/StatusBar/StatusBar.h
+++ Software/X-Track/USER/App/Pages/StatusBar/StatusBar.h
@@ -25,50 +25,11 @@
 
 #include "lvgl/lvgl.h"
 
-#ifdef __cplusplus
 namespace Page
 {
-
-class StatusBar
-{
-public:
-    static lv_obj_t* Create(lv_obj_t* par);
-
-    struct UI_t
-    {
-        lv_obj_t *cont;
-
-        struct
-        {
-            lv_obj_t *img;
-            lv_obj_t *label;
-        } satellite;
-
-        lv_obj_t *imgSD;
-
-        lv_obj_t *labelClock;
-
-        lv_obj_t *labelRec;
-
-        struct
-        {
-            lv_obj_t *img;
-            lv_obj_t *objUsage;
-            lv_obj_t *label;
-        } battery;
-
-        lv_obj_t *configWindow;
-    };
-
-    UI_t ui;
-};
 
 lv_obj_t* StatusBar_Create(lv_obj_t* par);
 
 }
-#else
-/* C 兼容：仅暴露创建接口（模拟器/部分工具可能用 C 解析头文件） */
-lv_obj_t* StatusBar_Create(lv_obj_t* par);
-#endif
 
 #endif
